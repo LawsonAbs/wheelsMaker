9226ee20814b66ca780ff753e1737aed66a2fb28
LawsonAbs <shenliu@ahnu.edu.cn>
2021-08-25 21:54:17 +0800

    delete invalid files

--- text_cls/config.py
+++
@@ -1,8 +0,0 @@
-"""获取配置相关
-"""
-PRETRAINED_MODEL_NAME = "bert-base-chinese"  # 使用 bert-base-chinese 作为预训练的模型
-BATCH_SIZE = 5  # 小批次
-TRAIN_EPOCH = 10
-TEST_EPOCH = 10
-MAX_LENGTH = 200
-LR = 1e-4  # learning rate
